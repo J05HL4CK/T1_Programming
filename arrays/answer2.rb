8114c732696a432bdf3fd0be4616ef42dae006ec
J05HL4CK <joshualack994@gmail.com>
2022-03-15 21:21:13 +1000

    answer 2 ruby ed

---
+++ arrays/answer2.rb
@@ -0,0 +1,11 @@
+# Prompt user for their age
+puts 'what is your current age?'
+# Cahnge value to integer
+years = gets.chomp.to_i
+# Prompt user for citizenship status
+puts 'Are you a citizen? Please enter (y/n)'
+# remove linebreak and trailing, letter to lowercase
+status = gets.chomp.downcase
+# Check if user is eligible based on criteria and print result to screen
+puts (years>=18 && status == 'y')? 'You\'re eligible to vote!' : 'You\'re ineligbile'
+# End program
