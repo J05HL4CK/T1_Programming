478ce45947b5ef60fd92291284a7e573b311ee66
J05HL4CK <joshualack994@gmail.com>
2022-03-16 15:19:36 +1000

    ruby method arguments

---
+++ ed-rb/method_arguments.rb
@@ -0,0 +1,11 @@
+#method is defined (as instructions to cook)
+def cook(item, time)
+    puts 'place pan on stove'
+    puts 'put water in pan'
+    puts "place #{item} in water and cook for #{time} minutes"
+    puts 'meal is ready'
+end
+
+# the same arguments are passed on the parameters we set
+cook('lizard meat', 5)
+cook('eye of neut', 8)
