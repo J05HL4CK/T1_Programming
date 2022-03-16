f09a3adda2bdb551065d1b7cbcc2b64fcdec0371
J05HL4CK <joshualack994@gmail.com>
2022-03-16 15:03:01 +1000

    ruby ed work

---
+++ ed-rb/methods.rb
@@ -0,0 +1,16 @@
+# method definition
+def tie_my_shoes
+    puts 'take hold of laces'
+    puts 'tie in a knot'
+    puts 'end' 
+end
+
+
+#method defined does not execute unless invoked 
+tie_my_shoes
+
+
+# methods can be called any number of times in a program 
+tie_my_shoes
+tie_my_shoes
+tie_my_shoes
