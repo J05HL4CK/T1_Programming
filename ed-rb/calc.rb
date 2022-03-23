9e04624814551241e1869344b95b09dc4b78475d
J05HL4CK <joshualack994@gmail.com>
2022-03-23 13:20:14 +1000

    calulator

---
+++ ed-rb/calc.rb
@@ -0,0 +1,25 @@
+module Calculator
+  def sum_difference
+    #return {Calculator.sum(n1, n2), Calculator.difference(n1, n2)}
+  end
+
+  def Calculator.sum
+    return n1 + n2
+  end
+
+  def Calculator.difference
+    return n1 - n2
+  end
+
+  def Calculator.product
+    return n1 * n2
+  end
+
+  def Calculator.quotient
+    return n1/n2
+  end
+end
+
+
+
+
