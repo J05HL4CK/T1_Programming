21e938b856071668836854611a6d6e8053d67da0
J05HL4CK <joshualack994@gmail.com>
2022-03-23 15:12:09 +1000

    banking app changes to advanced

--- ed-rb/bankapp.rb
+++ ed-rb/bankapp.rb
@@ -1,18 +1,44 @@
 def banking_app
-    # defined variables for menu
-    credit = 0
-    welc_greet = "welcome to the bank app"
-    bal_greet = 'Your current balance is $' + credit.to_s
-    opt_string = 'Please choose from the following options'
-    Options = [credit, ]
-    # end 
-    #start menu
-    puts welc_greet
-    puts bal_greet
-    puts 'What would you like to do?'
-    puts opt_string
-    for i in Options puts i
+    balance  = 0 
+    puts "Welcome to Lack's Bank"
+    # display balance to user
+    puts "Your balance is $#{balance}"
+    puts "What would you like to do?"
+    puts "Please select one of the following options to continue"
+    print %w(balance withdraw deposit)
+    # get user input
+    user_input = gets.chomp 
+    puts "You have selected #{user_input}"
+    case user_input
+    when "balance"
+        puts "Your balance is $#{balance}"
+    when "deposit"
+        # ask user how much to depos
+        puts "How much would you like to deposit?"
+        # get amount from user
+        puts "Please enter an amount"
+        depos_amount = gets.chomp.to_f
+        # update balance
+        balance += depos_amount
+        #display to user
+        puts "Your balance is $#{balance}"
+    when "withdraw"
+        #ask user for withdrawal amount
+        puts "Please enter a withdrawal amount"
+        #get user amount 
+        with_amnt = gets.chomp.to_f
+        #check if balance is greater than withdraw
+            #if yes minus from balance
+             # print balance to screen
+            #if no display to user insufficient funds 
+
+    else 
+        puts "Invalid selection"
     end
+
+    
+    
+
 
     
 
