f81df7d9cf98f3d3d349d9334dfce55d405599d4
J05HL4CK <joshualack994@gmail.com>
2022-03-23 22:32:12 +1000

    added string syntax to arrays lesson

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -1,16 +1,17 @@
-grocery_list=["milk","bread","eggs"]
-p grocery_list
+grocery_list = ["milk","bread","eggs"]
+## p grocery_list
+words =  %w(one two three four)
+## puts words
+## p grocery_list.class
+# syntax for a string (single quotes)
+lizard = %q(this is a string?)
 
+puts lizard.class
 
+## double quote string
+penguin_meat = %Q{This is also a stringlet bruhhhhh}
 
-
-
-
-
-
-
-
-
+puts penguin_meat
 
 
 
