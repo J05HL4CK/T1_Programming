c4956c03e2cabc8fa1622b9343fbab89f0714bca
J05HL4CK <joshualack994@gmail.com>
2022-03-24 15:49:25 +1000

    method notes BBB 1

---
+++ methods/method.rb
@@ -0,0 +1,10 @@
+# define the method with def and name it with snake_case (name should be descriptive of what it does)
+def add_sugar
+    puts %Q{Get a teaspoon of sugar}
+    puts %Q{Drop sugar into coffee}
+end
+
+# invoke it to run
+add_sugar
+add_sugar
+puts %Q{stirrrrrrr it up boieeeeeeee}
